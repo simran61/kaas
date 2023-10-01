78704ebe5cda4139c2032c6cf614adcb6f4bfadf
Simran Gangwani <simrangngwani3107@gmail.com>
2023-10-01 16:31:56 +0530

    header carousel

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import Motto from "../../components/Motto/Motto";
 import Headstart from "../../components/Headstart/Headstart";
 import CompanyWorks from "../../components/CompanyWork/CompanyWorks";
+import Carousel from "../../components/Carousel/Carousel";
 
 export default function Home() {
   return (
     <>
+      <Carousel />
       <CompanyWorks />
       <Headstart />
       <Motto />
